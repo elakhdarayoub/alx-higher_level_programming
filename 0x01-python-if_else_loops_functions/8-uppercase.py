0628f54242d7202cd719525f4489dd345deb791f
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-03 23:28:21 +0100

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    copy = ''
+    for c in str:
+        if ord(c) in range(97, 123):
+            copy += chr(ord(c) - 32)
+        else:
+            copy += c
+    print(copy)
