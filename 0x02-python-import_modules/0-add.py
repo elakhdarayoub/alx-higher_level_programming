e7673c7aecdbfe247cad736c7454049883460ea1
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-05 00:04:32 +0100

    task 0 is working ?

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,12 +2,11 @@
 import add_0
 
 
-def main():
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
 
 if __name__ == "__main__":
-    main()
+    pass
