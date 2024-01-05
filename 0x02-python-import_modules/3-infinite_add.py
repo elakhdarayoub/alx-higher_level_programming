47d1343a524ebe5cdb4b6a5602ae2abf97ad0a5d
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-05 02:23:07 +0100

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        result = 0
+        for i in sys.argv[1:]:
+            result += int(i)
+        print(result)
+    
