c3a7c23f5e2956edcec1549dcb890e98c365fdc8
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-07 01:28:56 +0100

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = []
+
+    # Checking tuple_a
+    if len(tuple_a) < 1:
+        tuple_a = (0, 0)
+    elif len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
+
+    # Checking tuple_b
+    if len(tuple_b) < 1:
+        tuple_b = (0, 0)
+    elif len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+
+    for tup in zip(tuple_a, tuple_b):
+        result.append(tup[0] + tup[1])
+    return tuple(result)
+
+# tuple_a = (1, 89)
+# tuple_b = (88, 11)
+# new_tuple = add_tuple(tuple_a, tuple_b)
+# print(new_tuple)
+
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
