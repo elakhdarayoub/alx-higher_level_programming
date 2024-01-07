69b68a7b30fd878e662b1898831cd698c1e488d9
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-07 01:35:48 +0100

    one checker down

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -19,10 +19,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         result.append(tup[0] + tup[1])
     return tuple(result)
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
+# tuple_a = (1, 89)
+# tuple_b = (88, 11)
+# new_tuple = add_tuple(tuple_a, tuple_b)
+# print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
