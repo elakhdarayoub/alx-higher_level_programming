e00b9ed997bd56ba78dc7900d88cf3b5e71cf255
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-10 23:56:30 +0100

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for item in sorted(a_dictionary):
-        print("{} : {}".format(item, a_dictionary[item]))
+        print(f"{item}: {a_dictionary[item]}")
