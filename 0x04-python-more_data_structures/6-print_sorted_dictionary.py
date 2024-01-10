27b9aed456ef05f3e10f8d92686e62fd8e8fe959
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-10 23:34:09 +0100

    task 6 - removed test

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,6 +3,3 @@
 def print_sorted_dictionary(a_dictionary):
     for item in sorted(a_dictionary):
         print(f"{item} : {a_dictionary[item]}")
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
