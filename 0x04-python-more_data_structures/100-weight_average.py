3b66ba837eb9d4bfbe17feb8059f1230677d65a4
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-11 00:40:43 +0100

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total = 0
+    divisor = 0
+
+    for item in list(map(lambda tup: tup[0] * tup[1], my_list)):
+        total += item
+    for element in list(map(lambda y: y[1], my_list)):
+        divisor += element
+    return total / divisor
+
+
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+result = weight_average(my_list)
+print("Average: {:0.2f}".format(result))
