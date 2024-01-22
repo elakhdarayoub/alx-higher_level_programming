27ff660042d9f718076b47466513284b43c7c8d8
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-22 22:33:45 +0100

    task 101 - a fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,7 +7,6 @@ def safe_function(fct, *args):
     try:
         result = fct(args[0], args[1])
         return result
-    except (ValueError, TypeError, IndexError,
-            ZeroDivisionError, NameError) as err:
+    except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
         return None
