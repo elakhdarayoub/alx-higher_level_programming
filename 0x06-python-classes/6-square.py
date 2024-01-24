8821396c884acd2cc8a6f98a6524aa03f5fc6c0e
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-01-24 01:17:12 +0100

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""A module about a square mob"""
+
+
+class Square:
+    """constructor"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    """Size getter"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Size setter"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value <= 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Position getter"""
+    @property
+    def position(self):
+        return self.__position
+
+    """Position setter"""
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple) or
+            len(value) < 2 or
+            value[0] < 0 or
+            value[1] < 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    """Returns the square area"""
+    def area(self):
+        return self.__size * self.__size
+
+    """Prints the square"""
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+        else:
+            for _ in range(0, self.__size):
+                if self.__position[1] == 0 and self.__position[0] > 0:
+                    for _ in range(0, self.__position[0]):
+                        print(" ", end='')
+                for _ in range(0, self.__size):
+                    print("#", end='')
+                print()
