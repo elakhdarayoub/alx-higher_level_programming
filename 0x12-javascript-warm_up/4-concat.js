a9f4d1e94fbb646d22734de05b6fbdefabfa7ffe
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-06-18 21:20:25 +0100

    file 4 fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,8 @@
 const args = process.argv;
 if (args.length < 4) {
   console.log('Not enough argument');
+} else if (args.length > 4) {
+    console.log("Too much arguments");
 } else {
   console.log(args[2] + ' is ' + args[3]);
 }
