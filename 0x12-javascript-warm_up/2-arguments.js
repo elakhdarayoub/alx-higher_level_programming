1c2fd7cbe4fd2db1460a503f3d8965dda5c951da
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2024-06-18 21:03:47 +0100

    file 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const number = process.argv.length;
+if (number <= 2) {
+  console.log('No argument');
+} else if (number === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
